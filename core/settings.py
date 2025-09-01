a0f96c7f89957530406d18a4305dd1e7894b9d65
Nobutann <luizgonzagafilho201365@gmail.com>
2025-09-01 16:09:35 -0300

    mudando nome pasta core

--- core/settings.py
+++ core/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'otakuverse.urls'
+ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
     {
@@ -66,7 +66,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'otakuverse.wsgi.application'
+WSGI_APPLICATION = 'core.wsgi.application'
 
 
 # Database
