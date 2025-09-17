9f387124b05d1973b5778f8aa82e1fc907fcbd4c
Nobutann <luizgonzagafilho201365@gmail.com>
2025-09-17 10:11:39 -0300

    criado signals.py e iniciado views do app users

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
 
-# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        password_confirm = request.POST.get('password_confirm')
+
+        if not all([username, email, password, password_confirm]):
+            return render(request, 'users/login.html', {'error': 'Todos os campos precisam ser preenchidos'})
+        if password != password_confirm:
+            return render(request, 'users/login.html', {'error': 'As senhas não correspondem'})
