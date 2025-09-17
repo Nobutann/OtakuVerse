ea0b8f692693bf741d33fae9bbe4934d21441672
Nobutann <luizgonzagafilho201365@gmail.com>
2025-09-17 09:01:16 -0300

    alterando core urls

--- core/urls.py
+++ core/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('busca/', include('anime.urls')),
+    path('animes/', include('animes.urls')),
 ]
