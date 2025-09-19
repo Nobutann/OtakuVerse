b6b85adb32e5294e9eac0d4ee5fbc4e6e7592f1a
Nobutann <luizgonzagafilho201365@gmail.com>
2025-09-19 10:58:12 -0300

    criando admin.py do app users

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,30 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import UserProfile, Friendship, FriendRequest
 
-admin.site.register(UserProfile)
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'bio', 'isPublic', 'animeCount', 'joinedDate']
+    list_filter = ['isPublic', 'showStats', 'gender', 'joinedDate']
+    search_fields = ['user__username', 'bio']
+    readonly_fields = ['joinedDate', 'lastOnline']
+
+@admin.register(Friendship)
+class FriendshipAdmin(admin.ModelAdmin):
+    list_display = ['from_user', 'to_user', 'status', 'timestamp']
+    list_filter = ['status', 'timestamp']
+    search_fields = ['from_user__username', 'to_user__username']
+    actions = ['accept', 'reject']
+
+    def accept(self, request, queryset):
+        for friend_request in queryset:
+            if friend_request.status == 'pending':
+                friend_request.accept()
+    
+    accept.short_description = "Aceitar pedidos"
+
+    def reject(self, request, queryset):
+        for friend_request in queryset:
+            if friend_request.status == 'pending':
+                friend_request.reject()
+
+    reject.short_description = "Rejeitar pedidos"
