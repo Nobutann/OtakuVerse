84d9bb0debf52751e6962e304e454d50e46bb879
Luiz Gonzaga <luizgonzagafilho201365@gmail.com>
2025-11-22 13:09:26 -0200

    corrigindo erro nos testes automatizados

--- cypress/e2e/favorite_characters.cy.js
+++ cypress/e2e/favorite_characters.cy.js
@@ -12,40 +12,45 @@ describe('Adicionar Personagem aos Favoritos', () => {
 
   it('deve criar conta, pesquisar personagem, adicionar e remover dos favoritos', () => {
     cy.visit('/users/signup/');
-    
     cy.get('#username').type(testUsername);
     cy.get('#email').type(testEmail);
     cy.get('#password').type(testPassword);
     cy.get('#password_confirm').type(testPassword);
     cy.get('.submit-btn').click();
-
+    
     cy.url().should('include', '/users/login/');
-
+    
     cy.get('#username').type(testUsername);
     cy.get('#password').type(testPassword);
     cy.get('.submit-btn').click();
-
+    
     cy.url().should('include', '/');
-
-    cy.get('#searchInput').type('Shinobu Oshino');
-    cy.get('input[name="search_type"][value="character"]').check();
-    cy.get('.search-btn').click();
-
-    cy.url().should('include', '/search/characters/');
-    cy.get('.results-query').should('contain', 'Shinobu Oshino');
-    cy.get('.char-card').should('exist');
-    cy.contains('.char-card h3', 'Shinobu Oshino').should('be.visible');
-
-    cy.contains('.char-card', 'Shinobu Oshino').find('.btn-add').click();
-
-    cy.url().should('include', '/lists/personagens/');
-    cy.contains('.char-card h3', 'Shinobu Oshino').should('be.visible');
-
+    
+    cy.visit('/lists/personagens/');
+    cy.wait(1000);
+    
+    cy.contains('button', 'Adicionar Personagens').click();
+    cy.wait(500);
+    
+    cy.get('#character-search-input').should('be.visible').type('Shinobu Oshino');
+    cy.contains('button', 'Buscar').click();
+    
+    cy.wait(3000);
+    
+    cy.get('#results .char-card', { timeout: 10000 }).should('exist');
+    cy.get('#results').contains('.char-card h3', 'Shinobu Oshino').should('be.visible');
+    cy.get('#results').contains('.char-card', 'Shinobu Oshino').find('.btn-add').click();
+    
+    cy.wait(2000);
+    
+    cy.get('#favorites .char-card').contains('h3', 'Shinobu Oshino').should('be.visible');
+    
     cy.on('window:confirm', () => true);
-
-    cy.contains('.char-card', 'Shinobu Oshino').find('.btn-remove').click();
-
-    cy.contains('.char-card h3', 'Shinobu Oshino').should('not.exist');
+    cy.get('#favorites').contains('.char-card', 'Shinobu Oshino').find('.btn-remove').click();
+    
+    cy.wait(1000);
+    
+    cy.get('#favorites').should('not.contain', 'Shinobu Oshino');
     cy.get('.empty-list-message').should('be.visible');
   });
 });
